60e84f1323a0198e73c3d5dd4d8d8795e144804e
Stanislav Rostov <SRostov@doitt.nyc.gov>
2020-05-21 16:59:02 -0400

    updated by Stan

---
+++ TestConsoleApp1/TestConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Stan!");
+        }
+    }
+}
